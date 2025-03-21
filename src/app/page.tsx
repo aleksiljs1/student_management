232ec9cece0d22503bcca07358a640276b247527
aleksiljs1 <aleksanderiljas@gmail.com>
2025-03-21 16:53:05 +0100

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import StudentBoard from "@/components/student-board";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
 import React from "react";
+import { urlConst } from "@/consts/path-consts";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
+  const router = useRouter();
+  router.push(urlConst.dashboardRedirect);
   return;
   <StudentBoard />;
 }
